21d4625bf6c7da1fac247d0e4da9b301f2e1b653
ElBelloMango <miguel.gallego@upb.edu.co>
2021-02-09 22:05:18 -0500

    implementacion mkreg y savedb

--- proyectos/proyecto1BD/database.c
+++ proyectos/proyecto1BD/database.c
@@ -7,48 +7,90 @@ typedef struct estudiante
     char nombre[30];
     int semestre;
 };
-
-struct estudiante* mkdb(char* nombre,int size);
-struct estudiante* mkdb(char* nombre,int size)
+void mkreg(struct estudiante *bd, int cedula, char *nombre, int semestre, int *nroRegistro);
+void mkreg(struct estudiante *bd, int cedula, char *nombre, int semestre, int *nroRegistro)
 {
-    return (struct estudiante *)malloc(sizeof(struct estudiante)*size);
+    strcpy((bd + *nroRegistro)->nombre, nombre);
+    (bd + *nroRegistro)->cedula = cedula;
+    (bd + *nroRegistro)->semestre = semestre;
+    (*nroRegistro)++;
+}
+struct estudiante* mkdb(char *nombre, int size);
+struct estudiante* mkdb(char *nombre, int size)
+{
+    return (struct estudiante *)malloc(sizeof(struct estudiante) * size);
 }
 
 int main(int argc, char const *argv[])
 {
+    int contadorRegistro = 0;
+    int tamanobd = 0;
     char comando[20];
-    printf("Ingrese un comando\n");
-    scanf("%s",comando);
-    getc(stdin);
-    printf("%s\n",comando);
-    struct estudiante *espaciobd;
-    if (strncmp("mkdb",comando,4)==0)
+    do
     {
-        char nombre[20];
-        int tamano;
-        fscanf(stdin,"%s",nombre);
-        fscanf(stdin,"%d",&tamano);
-        fgets("nan","%s[^\n]",stdin); //Eliminar argumentos extras.
-        espaciobd = mkdb(nombre,tamano);
-         
-        strcpy(espaciobd->nombre, "Camilo" );
-        espaciobd->cedula=122021221;
-        espaciobd->semestre=3;
-        for (int i = 0; i < tamano; i++)
+        printf("Ingrese un comando\n");
+        fscanf(stdin,"%s", comando);
+        getc(stdin);
+        struct estudiante *espaciobd;
+        if (strncmp("mkdb", comando,4) == 0)
         {
-            printf("%s\n",(espaciobd+i)->nombre);
-            printf("%d\n",(espaciobd+i)->cedula);
-            printf("%d\n",(espaciobd+i)->semestre);
+            char nombre[20];
+            fscanf(stdin, "%s", nombre);
+            fscanf(stdin, "%d", &tamanobd);
+            fgets("nan", "%s[^\n]", stdin); //Eliminar argumentos extras.
+            espaciobd = mkdb(nombre, tamanobd);
         }
-        
-    }
-    else if(strncmp("loaddb",comando,6)==0)
-    {
-        char nombre[20];
-        fscanf(stdin,"%s",nombre);
-        fgets("nan","%s[^\n]",stdin); //Eliminar argumentos extras.
-    }
-    
-    
+        else if (strncmp("mkreg", comando, 5) == 0)
+        {
+            char nombre[20];
+            int cedula;
+            int semestre;
+            fscanf(stdin, "%s", nombre);
+            fscanf(stdin, "%d", &cedula);
+            fscanf(stdin, "%d", &semestre);
+            fgets("nan", "%s[^\n]", stdin); //Eliminar argumentos extras.
+            mkreg(espaciobd, cedula, nombre, semestre, &contadorRegistro);
+        }
+        else if (strncmp("savedb", comando, 6) == 0)
+        {
+            char nombre[20];
+            fscanf(stdin, "%s", nombre);
+            fgets("nan", "%s[^\n]", stdin); //Eliminar argumentos extras.
+            FILE *archivo2=fopen(nombre,"w+");
+            fprintf(archivo2,"|Nombre |Cedula |Semestre |\n");
+            for (int i = 0; i < contadorRegistro; i++)
+            {
+                fprintf(archivo2, "%s %d %d\n", (espaciobd + i)->nombre, (espaciobd + i)->cedula, (espaciobd + i)->semestre);
+            }
+        }
+        else if (strncmp("loaddb", comando, 6) == 0)
+        {
+            
+            char nombre[20];
+            fscanf(stdin, "%s", nombre);
+            fgets("nan", "%s[^\n]", stdin); //Eliminar argumentos extras.
+            char linea[50];
+            FILE *archivo=fopen(nombre,"r");
+            fgets("nan", "%s[^\n]", archivo); //Omitir primera linea.
+            fgets(linea[50], "%s[^\n]", archivo);
+            printf("%s\n",linea);
+        }
+        else if (strncmp("readall", comando, 7) == 0)
+        {
+            for (int i = 0; i < tamanobd; i++)
+            {
+                printf("%s\n", (espaciobd + i)->nombre);
+                printf("%d\n", (espaciobd + i)->cedula);
+                printf("%d\n", (espaciobd + i)->semestre);
+            }
+        }
+
+        else if (strncmp("exit", comando, 4) == 0)
+        {
+            break;
+        }
+
+    } while (1);
+
     return 0;
 }
