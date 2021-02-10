3434e71811e9c3ebb3899d6de33e01c0366f02f1
ElBelloMango <miguel.gallego@upb.edu.co>
2021-02-10 10:02:39 -0500

    loaddb implementado, bug de fgets pospuesto

--- proyectos/proyecto1BD/database.c
+++ proyectos/proyecto1BD/database.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 typedef struct estudiante
 {
     int cedula;
     char nombre[30];
     int semestre;
 };
+
 void mkreg(struct estudiante *bd, int cedula, char *nombre, int semestre, int *nroRegistro);
 void mkreg(struct estudiante *bd, int cedula, char *nombre, int semestre, int *nroRegistro)
 {
@@ -26,18 +28,18 @@ int main(int argc, char const *argv[])
     int contadorRegistro = 0;
     int tamanobd = 0;
     char comando[20];
+    struct estudiante *espaciobd;
     do
     {
         printf("Ingrese un comando\n");
         fscanf(stdin,"%s", comando);
         getc(stdin);
-        struct estudiante *espaciobd;
         if (strncmp("mkdb", comando,4) == 0)
         {
             char nombre[20];
             fscanf(stdin, "%s", nombre);
-            fscanf(stdin, "%d", &tamanobd);
-            fgets("nan", "%s[^\n]", stdin); //Eliminar argumentos extras.
+            fscanf(stdin, "%d", &tamanobd); 
+            // fgets("nan", "%s[^\n]", stdin); //Eliminar argumentos extras.
             espaciobd = mkdb(nombre, tamanobd);
         }
         else if (strncmp("mkreg", comando, 5) == 0)
@@ -48,15 +50,16 @@ int main(int argc, char const *argv[])
             fscanf(stdin, "%s", nombre);
             fscanf(stdin, "%d", &cedula);
             fscanf(stdin, "%d", &semestre);
-            fgets("nan", "%s[^\n]", stdin); //Eliminar argumentos extras.
+            // fgets("nan", "%s[^\n]", stdin); //Eliminar argumentos extras.
             mkreg(espaciobd, cedula, nombre, semestre, &contadorRegistro);
         }
         else if (strncmp("savedb", comando, 6) == 0)
         {
             char nombre[20];
             fscanf(stdin, "%s", nombre);
-            fgets("nan", "%s[^\n]", stdin); //Eliminar argumentos extras.
+            // fgets("nan", "%s[^\n]", stdin); //Eliminar argumentos extras.
             FILE *archivo2=fopen(nombre,"w+");
+            fprintf(archivo2,"Tamaño: %d\n",tamanobd);
             fprintf(archivo2,"|Nombre |Cedula |Semestre |\n");
             for (int i = 0; i < contadorRegistro; i++)
             {
@@ -68,12 +71,46 @@ int main(int argc, char const *argv[])
             
             char nombre[20];
             fscanf(stdin, "%s", nombre);
-            fgets("nan", "%s[^\n]", stdin); //Eliminar argumentos extras.
+            // fgets("nan", "%s[^\n]", stdin); //Eliminar argumentos extras.
+            char LTrash[50];
             char linea[50];
+            char lineaAnt[50];
+            char nombreEstudiante[30];
+            int contRegistros = 0;
+            int cedula;
+            int semestre;
             FILE *archivo=fopen(nombre,"r");
-            fgets("nan", "%s[^\n]", archivo); //Omitir primera linea.
-            fgets(linea[50], "%s[^\n]", archivo);
-            printf("%s\n",linea);
+            fscanf(archivo,"%s",LTrash); //Omitimos la primer palabra
+            fscanf(archivo,"%d",&tamanobd); //guardamos el tamaño de la BD
+            fgets(lineaAnt, "%s[^\n]", archivo); //Omitir primera linea.
+            fgets(lineaAnt, "%s[^\n]", archivo); //Omitir Segunda linea.
+            while (1)
+            {
+                fgets(linea, "%s[^\n]", archivo);
+                if (strncmp(lineaAnt, linea, 50)==0)
+                {
+                    break;
+                }
+                printf("%s\n",linea);
+                strcpy(lineaAnt,linea);
+                contRegistros++;
+            }
+            contadorRegistro = contRegistros;
+            printf("%d\n",contRegistros);
+            espaciobd = mkdb(nombre, tamanobd);
+            rewind(archivo);
+            fgets(lineaAnt, "%s[^\n]", archivo); //Omitir primera linea.
+            fgets(lineaAnt, "%s[^\n]", archivo); //Omitir Segunda linea.
+            for (int i = 0; i < contRegistros;i++)
+            {
+                fscanf(archivo,"%s",nombreEstudiante);
+                fscanf(archivo,"%d",&cedula);
+                fscanf(archivo,"%d",&semestre);
+                // mkreg(espaciobd, cedula, nombreEstudiante, semestre, &i);
+                strcpy((espaciobd + i)->nombre, nombreEstudiante);
+                (espaciobd + i)->cedula = cedula;
+                (espaciobd + i)->semestre = semestre;
+            }
         }
         else if (strncmp("readall", comando, 7) == 0)
         {
